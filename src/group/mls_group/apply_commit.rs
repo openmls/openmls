d7b8048232838b542a9b9cde42a89266a935a078
raphaelrobert <raphaelrobert@users.noreply.github.com>
2020-12-15 11:26:51 +0100

    Redundant ciphertexts (#262)
    * Redundant ciphertexts
    
    * Address review comments
    
    Co-authored-by: Konrad Kohbrok <konrad.kohbrok@datashrine.de>

--- src/group/mls_group/apply_commit.rs
+++ src/group/mls_group/apply_commit.rs
@@ -46,14 +46,14 @@ impl MlsGroup {
 
         // Create provisional tree and apply proposals
         let mut provisional_tree = self.tree.borrow_mut();
-        let (path_required_by_commit, group_removed, _invited_members) =
+        let apply_proposals_values =
             match provisional_tree.apply_proposals(proposal_queue, own_key_packages) {
                 Ok(res) => res,
                 Err(_) => return Err(ApplyCommitError::OwnKeyNotFound),
             };
 
         // Check if we were removed from the group
-        if group_removed {
+        if apply_proposals_values.self_removed {
             return Err(ApplyCommitError::SelfRemoved);
         }
 
@@ -83,12 +83,19 @@ impl MlsGroup {
                 };
                 provisional_tree.replace_private_tree(own_kpb, &serialized_context)
             } else {
+                // Collect the new leaves' indexes so we can filter them out in the resolution
+                // later.
                 provisional_tree
-                    .update_path(sender, &path, &serialized_context)
+                    .update_path(
+                        sender,
+                        &path,
+                        &serialized_context,
+                        apply_proposals_values.exclusion_list(),
+                    )
                     .unwrap()
             }
         } else {
-            if path_required_by_commit {
+            if apply_proposals_values.path_required {
                 return Err(ApplyCommitError::RequiredPathNotFound);
             }
             &zero_commit_secret
