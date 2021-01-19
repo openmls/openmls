5da36bd5dca6125e966cd1e4a4c62f2f308b6885
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2021-01-19 16:11:06 +0100

    add some more documentation

--- tests/utils/mod.rs
+++ tests/utils/mod.rs
@@ -1,2 +1,4 @@
+/// A framework to create integration tests of managed group API.
 pub mod managed_utils;
+/// A framework to create integration tests of the "raw" mls_group API.
 pub mod mls_utils;
