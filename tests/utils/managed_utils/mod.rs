7fd94bf6fe3378651dbd517311f9f703a75cc23d
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2021-01-21 09:22:57 +0100

    extend the check_group_state function to also check the exporter

--- tests/utils/managed_utils/mod.rs
+++ tests/utils/managed_utils/mod.rs
@@ -73,6 +73,7 @@ pub struct Group {
     pub ciphersuite: Ciphersuite,
     pub group_config: ManagedGroupConfig,
     pub public_tree: Vec<Option<Node>>,
+    pub exporter_secret: Vec<u8>,
 }
 
 impl Group {
@@ -268,12 +269,13 @@ impl<'ks> ManagedTestSetup<'ks> {
             println!("Index: {:?}, Id: {:?}", index, member_id);
         }
         group.public_tree = sender_group.export_ratchet_tree();
+        group.exporter_secret = sender_group.export_secret("test", 32)?;
         Ok(())
     }
 
-    /// Check if the public tree of the given group is the same in the group
-    /// state of each group member. This function panics if that is not the
-    /// case.
+    /// Check if the public tree and the exporter secret with label "test" and
+    /// length of the given group is the same for each group member. This
+    /// function panics if that is not the case.
     pub fn check_group_states(&self, group: &Group) {
         let clients = self.clients.borrow();
         for (_, m_id) in &group.members {
@@ -282,6 +284,10 @@ impl<'ks> ManagedTestSetup<'ks> {
             // Some group members may not have received their welcome messages yet.
             if let Some(group_state) = group_states.get(&group.group_id) {
                 assert_eq!(group_state.export_ratchet_tree(), group.public_tree);
+                assert_eq!(
+                    group_state.export_secret("test", 32).unwrap(),
+                    group.exporter_secret
+                );
             };
         }
     }
@@ -349,6 +355,7 @@ impl<'ks> ManagedTestSetup<'ks> {
         let creator_groups = group_creator.groups.borrow();
         let group = creator_groups.get(&group_id).unwrap();
         let public_tree = group.export_ratchet_tree();
+        let exporter_secret = group.export_secret("test", 32)?;
         let mut member_ids = Vec::new();
         member_ids.push((0, group_creator_id));
         let group = Group {
@@ -357,6 +364,7 @@ impl<'ks> ManagedTestSetup<'ks> {
             ciphersuite: ciphersuite.clone(),
             group_config: self.default_mgc.clone(),
             public_tree,
+            exporter_secret,
         };
         groups.insert(group_id.clone(), group);
         Ok(group_id)
