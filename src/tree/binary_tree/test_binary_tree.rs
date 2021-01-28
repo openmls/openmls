2beb46d4ac344ffefcdd712f58ccaf446ebdd845
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2021-01-28 16:49:44 +0100

    remove some dead code

--- src/tree/binary_tree/test_binary_tree.rs
+++ src/tree/binary_tree/test_binary_tree.rs
@@ -60,25 +60,3 @@ fn test_out_of_bounds() {
 
     assert!(node.is_err());
 }
-
-//#[test]
-//fn test_resolution() {
-//    // Create simple tree.
-//    let nodes = vec![0, 1, 2, 3, 4, 5, 6];
-//    let tree = BinaryTree::from(nodes);
-//    // A simple predicate. Return true if the node equals 1 or if it's a leaf
-//    // node.
-//    let predicate = |index: NodeIndex, node: &u8| -> Vec<NodeIndex> {
-//        if node == &1u8 || index.is_leaf() {
-//            vec![index.to_owned()]
-//        } else {
-//            vec![]
-//        }
-//    };
-//
-//    let resolution = tree.resolve(&NodeIndex::from(3u32), &predicate).unwrap();
-//    println!("Resolution: {:?}", resolution);
-//    let mut result = vec![1u32, 4u32, 6u32];
-//    let result_indices: Vec<NodeIndex> = result.drain(..).map(|i| NodeIndex::from(i)).collect();
-//    assert_eq!(resolution, result_indices)
-//}
