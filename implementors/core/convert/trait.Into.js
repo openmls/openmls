578013062e2a6d3b6e5cbf9cd8f83ea8849a7f27
franziskuskiefer <franziskuskiefer@users.noreply.github.com>
2021-02-08 12:32:17 +0000

    deploy: c296536eee917bbd20347b5741cbfab760504f08

--- implementors/core/convert/trait.Into.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["openmls"] = [{"text":"impl Into&lt;u32&gt; for LeafIndex","synthetic":false,"types":[]},{"text":"impl Into&lt;usize&gt; for LeafIndex","synthetic":false,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
