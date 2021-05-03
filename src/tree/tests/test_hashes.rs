159ddd9054c5bf620980d819707e63c3fa595f78
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2021-05-03 15:13:33 +0200

    Some refactoring and simplification of the RatchetTree (#396)

--- src/tree/tests/test_hashes.rs
+++ src/tree/tests/test_hashes.rs
@@ -37,8 +37,7 @@ fn test_parent_hash() {
         // The first key package bundle is used for the tree holder
         let key_package_bundle = key_package_bundles.remove(0);
 
-        let mut tree =
-            RatchetTree::new_from_nodes(&ciphersuite, key_package_bundle, &nodes).unwrap();
+        let mut tree = RatchetTree::new_from_nodes(key_package_bundle, &nodes).unwrap();
 
         assert!(tree.verify_parent_hashes().is_ok());
 
