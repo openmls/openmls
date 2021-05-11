02cde8e73f11d4f8d3d36d45265261466639532d
Franziskus Kiefer <franziskuskiefer@gmail.com>
2021-05-11 16:58:22 +0200

    info is empty and aad is the group context

--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -354,7 +354,7 @@ impl RatchetTree {
         // Decrypt the secret and derive path secrets
         let secret_bytes =
             self.ciphersuite
-                .hpke_open(hpke_ciphertext, &private_key, group_context, &[])?;
+                .hpke_open(hpke_ciphertext, &private_key, &[], group_context)?;
         let path_secret =
             Secret::from_slice(&secret_bytes, ProtocolVersion::default(), self.ciphersuite).into();
         // Derive new path secrets and generate keypairs
@@ -519,8 +519,8 @@ impl RatchetTree {
                     let pk = self.nodes[index].public_hpke_key().unwrap();
                     self.ciphersuite.hpke_seal_secret(
                         &pk,
-                        group_context,
                         &[],
+                        group_context,
                         &path_secret.path_secret,
                     )
                 })
