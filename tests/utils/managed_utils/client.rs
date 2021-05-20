9f2019f0ca3e658ffcc71733747cc6c959982cba
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2021-05-20 08:33:47 +0200

    remove explicit activity check when processing message

--- tests/utils/managed_utils/client.rs
+++ tests/utils/managed_utils/client.rs
@@ -104,11 +104,6 @@ impl Client {
         let group_state = group_states
             .get_mut(&group_id)
             .ok_or(ClientError::NoMatchingGroup)?;
-        // Prevent feeding further messages to client after it was removed
-        // by one of the messages.
-        if !group_state.is_active() {
-            return Ok(());
-        }
         let events = group_state.process_message(message.clone())?;
         for event in events {
             match event {
