378c9befd0316fd9848fb21d10e8cf9e0f50f82b
kkohbrok <kkohbrok@users.noreply.github.com>
2021-08-02 08:39:48 +0000

    deploy: 2e02443ed755131a58d270b76e31547ef5378a0f

--- implementors/core/ops/deref/trait.Deref.js
+++ implementors/core/ops/deref/trait.Deref.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["openmls"] = [{"text":"impl&lt;'b&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"openmls/key_store/struct.CbGuard.html\" title=\"struct openmls::key_store::CbGuard\">CbGuard</a>&lt;'b&gt;","synthetic":false,"types":["openmls::key_store::CbGuard"]}];
+implementors["openmls"] = [{"text":"impl&lt;'b&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.54.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"openmls/key_store/struct.CbGuard.html\" title=\"struct openmls::key_store::CbGuard\">CbGuard</a>&lt;'b&gt;","synthetic":false,"types":["openmls::key_store::CbGuard"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
