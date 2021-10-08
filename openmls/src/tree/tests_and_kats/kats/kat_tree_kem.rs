50e0304b4328a38f9f0fa4922c449c5b763de0af
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2021-10-08 09:09:21 +0200

    Add interop client (#447)
    This commit adds the interop client and adds logging in some places.

--- openmls/src/tree/tests_and_kats/kats/kat_tree_kem.rs
+++ openmls/src/tree/tests_and_kats/kats/kat_tree_kem.rs
@@ -173,17 +173,24 @@ pub fn run_test_vector(test_vector: TreeKemTestVector) -> Result<(), TreeKemTest
     log::trace!("Common ancestor: {:?}", common_ancestor);
     let path = parent_direct_path(common_ancestor, tree_before.leaf_count()).unwrap();
     log::trace!("path: {:?}", path);
-    let mut start_secret: PathSecret =
-        Secret::from(hex_to_bytes(&test_vector.my_path_secret).as_slice()).into();
-    start_secret.config(ciphersuite, ProtocolVersion::default());
+    let start_secret: PathSecret = Secret::from_slice(
+        hex_to_bytes(&test_vector.my_path_secret).as_slice(),
+        ProtocolVersion::default(),
+        ciphersuite,
+    )
+    .into();
+
     tree_before
         .private_tree_mut()
         .continue_path_secrets(ciphersuite, start_secret, &path);
 
     // Check if the root secrets match up.
-    let mut root_secret_after_add: PathSecret =
-        Secret::from(hex_to_bytes(&test_vector.root_secret_after_add).as_slice()).into();
-    root_secret_after_add.config(ciphersuite, ProtocolVersion::default());
+    let root_secret_after_add: PathSecret = Secret::from_slice(
+        hex_to_bytes(&test_vector.root_secret_after_add).as_slice(),
+        ProtocolVersion::default(),
+        ciphersuite,
+    )
+    .into();
 
     if &root_secret_after_add
         != tree_before
@@ -214,9 +221,12 @@ pub fn run_test_vector(test_vector: TreeKemTestVector) -> Result<(), TreeKemTest
     // Rename to avoid confusion.
     let tree_after = tree_before;
     let root_secret_after = tree_after.private_tree().path_secrets().last().unwrap();
-    let mut root_secret_after_update: PathSecret =
-        Secret::from(hex_to_bytes(&test_vector.root_secret_after_update).as_slice()).into();
-    root_secret_after_update.config(ciphersuite, ProtocolVersion::default());
+    let root_secret_after_update: PathSecret = Secret::from_slice(
+        hex_to_bytes(&test_vector.root_secret_after_update).as_slice(),
+        ProtocolVersion::default(),
+        ciphersuite,
+    )
+    .into();
 
     if &root_secret_after_update != root_secret_after {
         if cfg!(test) {
