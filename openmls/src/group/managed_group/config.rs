50de0d96e99ca136b1ecd389c8149a7f48cf1930
raphaelrobert <git@raphaelrobert.com>
2021-10-11 15:11:35 +0200

    Minor fixes

--- openmls/src/group/managed_group/config.rs
+++ openmls/src/group/managed_group/config.rs
@@ -2,7 +2,7 @@ use super::*;
 
 use serde::{Deserialize, Serialize};
 
-/// Specifies the configuration parameters for a managed group
+/// Specifies the configuration parameters for a [`ManagedGroup`]
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct ManagedGroupConfig {
     /// Defines whether handshake messages (Proposals & Commits) are encrypted.
@@ -37,48 +37,49 @@ impl ManagedGroupConfig {
             callbacks: ManagedGroupCallbacks::default(),
         }
     }
+
     /// Sets the `wire_format` property of the ManagedGroupConfig.
     pub fn with_wire_format(mut self, wire_format: WireFormat) -> Self {
         self.wire_format = wire_format;
         self
     }
+
     /// Sets the `update_policy` property of the ManagedGroupConfig.
     pub fn with_update_policy(mut self, update_policy: UpdatePolicy) -> Self {
         self.update_policy = update_policy;
         self
     }
+
     /// Sets the `padding_size` property of the ManagedGroupConfig.
     pub fn with_padding_size(mut self, padding_size: usize) -> Self {
         self.padding_size = padding_size;
         self
     }
+
     /// Sets the `number_of_resumption_secrets` property of the ManagedGroupConfig.
     pub fn with_number_of_resumtion_secrets(mut self, number_of_resumption_secrets: usize) -> Self {
         self.number_of_resumption_secrets = number_of_resumption_secrets;
         self
     }
+
     /// Sets the `use_ratchet_tree_extension` property of the ManagedGroupConfig.
     pub fn with_use_ratchet_tree_extension(mut self, use_ratchet_tree_extension: bool) -> Self {
         self.use_ratchet_tree_extension = use_ratchet_tree_extension;
         self
     }
+
     /// Sets the `callbacks` property of the ManagedGroupConfig.
     pub fn with_callbacks(mut self, callbacks: ManagedGroupCallbacks) -> Self {
         self.callbacks = callbacks;
         self
     }
 
-    #[cfg(test)]
-    pub fn test_default() -> Self {
-        Self::new().with_wire_format(WireFormat::MlsPlaintext)
-    }
-
-    /// Gets the [`ManagedGroupConfig`] wire format.
+    /// Get the [`ManagedGroupConfig`] wire format.
     pub fn wire_format(&self) -> WireFormat {
         self.wire_format
     }
 
-    /// Get a reference to the [`ManagedGroupConfig`] padding size.
+    /// Get the [`ManagedGroupConfig`] padding size.
     pub fn padding_size(&self) -> usize {
         self.padding_size
     }
@@ -88,19 +89,24 @@ impl ManagedGroupConfig {
         &self.update_policy
     }
 
-    /// Get a reference to the [`ManagedGroupConfig`] number of resumption secrets.
-    pub fn number_of_resumption_secrets(&self) -> &usize {
-        &self.number_of_resumption_secrets
+    /// Get the [`ManagedGroupConfig`] number of resumption secrets.
+    pub fn number_of_resumption_secrets(&self) -> usize {
+        self.number_of_resumption_secrets
     }
 
-    /// Get a reference to the [`ManagedGroupConfig`] use ratchet tree extension.
-    pub fn use_ratchet_tree_extension(&self) -> &bool {
-        &self.use_ratchet_tree_extension
+    /// Get the [`ManagedGroupConfig`] boolean flage that indicates whether ratchet_tree_extension should be used.
+    pub fn use_ratchet_tree_extension(&self) -> bool {
+        self.use_ratchet_tree_extension
     }
 
-    /// Get a reference to the [`ManagedGroupConfig`] use rcallbacks.
+    /// Get a reference to the [`ManagedGroupConfig`] callbacks.
     pub fn callbacks(&self) -> &ManagedGroupCallbacks {
         &self.callbacks
+    }
+
+    #[cfg(test)]
+    pub fn test_default() -> Self {
+        Self::new().with_wire_format(WireFormat::MlsPlaintext)
     }
 }
 
