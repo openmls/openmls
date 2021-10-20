8681804161fe60370001b8fa30a6de73838324a6
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2021-10-20 13:29:03 +0200

    clean up/format python test script

--- interop_client/test_with_runner.py
+++ interop_client/test_with_runner.py
@@ -3,40 +3,45 @@
 import subprocess
 import os
 import atexit
-import shlex
 
 import socket
 
 owd = os.getcwd()
+
 
 # This function perform some cleanup. In particular, it removes the cloned
 # mls-implementations repo and kills the interop_client.
 def cleanup():
     os.chdir(owd)
     subprocess.run(['killall', 'interop_client'])
-    subprocess.run(['rm','-rf', 'mls-implementations'])
+    subprocess.run(['rm', '-rf', 'mls-implementations'])
+
 
 # Register that function to be run at exit.
 atexit.register(cleanup)
 
-# Compile and run the interop client, but suppress output. For now, we're only interested in the output of the test runner.
-interop_client_p = subprocess.Popen(['cargo', 'run', '--release'], stdout=subprocess.DEVNULL)
+# Compile and run the interop client, but suppress output. For now, we're only
+# interested in the output of the test runner.
+interop_client_p = subprocess.Popen(
+    ['cargo', 'run', '--release'], stdout=subprocess.DEVNULL)
 
 sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 
 # Wait for the process to either terminate or start listening at a port.
-while sock.connect_ex(('::1',50051)) != 0 & (interop_client_p.poll() is None): ()
-
-# If it terminated, something went wrong and we want to exit with code 1.
-if interop_client_p.poll() is not None:
-    print("client not running, aborting")
-    exit(1)
+while sock.connect_ex(('::1', 50051)) != 0:
+    # If the compilation process or the interop client itself terminates for
+    # some reason, we want to exit with code 1.
+    if interop_client_p.poll() is not None:
+        print("compilation failed or client has exited prematurely, aborting")
+        exit(1)
 
 # Clone the mls-implementations repo to build the test runner.
-subprocess.run(['git', 'clone', 'https://github.com/mlswg/mls-implementations.git'])
+subprocess.run(
+    ['git', 'clone', 'https://github.com/mlswg/mls-implementations.git'])
 
 # Copy the config.json to the place where the test runner expects it.
-subprocess.run(['cp', 'config.json', 'mls-implementations/interop/test-runner'])
+subprocess.run(
+    ['cp', 'config.json', 'mls-implementations/interop/test-runner'])
 
 # Change into the test runner dir.
 os.chdir("./mls-implementations/interop/test-runner")
