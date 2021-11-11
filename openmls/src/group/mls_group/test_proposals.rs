a3f5e3e2d98aaf854f7c599e98b89abc548c6110
raphaelrobert <git@raphaelrobert.com>
2021-11-11 15:19:40 +0100

    Remove duplicate test

--- openmls/src/group/mls_group/test_proposals.rs
+++ openmls/src/group/mls_group/test_proposals.rs
@@ -315,31 +315,3 @@ fn proposal_queue_order() {
         assert_eq!(proposal_collection[1].proposal(), &proposal_add_alice1);
     }
 }
-
-/// This test encodes and decodes the `ProposalOrRef` struct and makes sure the
-/// decoded values are the same as the original
-#[test]
-fn proposals_codec() {
-    let crypto = OpenMlsRustCrypto::default();
-    let ciphersuite =
-        &Ciphersuite::new(CiphersuiteName::MLS10_128_DHKEMX25519_AES128GCM_SHA256_Ed25519).unwrap();
-
-    // Proposal
-
-    let remove_proposal = RemoveProposal { removed: 123 };
-    let proposal = Proposal::Remove(remove_proposal);
-    let proposal_or_ref = ProposalOrRef::Proposal(proposal.clone());
-    let encoded = proposal_or_ref.tls_serialize_detached().unwrap();
-    let decoded = ProposalOrRef::tls_deserialize(&mut encoded.as_slice()).unwrap();
-
-    assert_eq!(proposal_or_ref, decoded);
-
-    // Reference
-
-    let reference = ProposalReference::from_proposal(ciphersuite, &crypto, &proposal).unwrap();
-    let proposal_or_ref = ProposalOrRef::Reference(reference);
-    let encoded = proposal_or_ref.tls_serialize_detached().unwrap();
-    let decoded = ProposalOrRef::tls_deserialize(&mut encoded.as_slice()).unwrap();
-
-    assert_eq!(proposal_or_ref, decoded);
-}
