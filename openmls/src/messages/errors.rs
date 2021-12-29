c6cf40764f15a71e1f56700c26be4af485d72b88
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2021-12-29 09:20:09 +0100

    Refactor proposal queues (#667)
    This commit refactors proposal queues and in the process
     - gets rid of the `CreationProposalQueue` and the associated implementations and structs
     - gets rid of the `apply_proposal` function (thus fixing #565)
     - Removes the `Staged...` prefix whenever it's followed by `Proposal...`, as these structs are now used both for creation and staging
    
    The advantage of the `CreationProposalQueue` was that it relied on references rather than full `Proposal` structs. However, in the course of #617 we started returning a `StagedProposalQueue` at the end of `create_commit`, so the cloning was taking place in any case.

--- openmls/src/messages/errors.rs
+++ openmls/src/messages/errors.rs
@@ -14,17 +14,6 @@ implement_error! {
 }
 
 implement_error! {
-    pub enum ProposalQueueError {
-        Simple {
-            ProposalNotFound = "Not all proposals in the Commit were found locally.",
-        }
-        Complex {
-            NotAProposal(QueuedProposalError) = "The given MLS Plaintext was not a Proposal.",
-        }
-    }
-}
-
-implement_error! {
     pub enum ProposalOrRefTypeError {
         UnknownValue = "Invalid value for ProposalOrRefType was found.",
     }
@@ -33,16 +22,5 @@ implement_error! {
 impl From<ProposalOrRefTypeError> for tls_codec::Error {
     fn from(e: ProposalOrRefTypeError) -> Self {
         tls_codec::Error::DecodingError(format!("{:?}", e))
-    }
-}
-
-implement_error! {
-    pub enum QueuedProposalError {
-        Simple {
-            WrongContentType = "API misuse. Only proposals can end up in the proposal queue",
-        }
-        Complex {
-            TlsCodecError(TlsCodecError) = "Error serializing",
-        }
     }
 }
