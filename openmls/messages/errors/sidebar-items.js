45c92602e0bae2ee8e4cc31e2156d7bcd9502217
kkohbrok <kkohbrok@users.noreply.github.com>
2021-12-29 08:25:43 +0000

    deploy: c6cf40764f15a71e1f56700c26be4af485d72b88

--- openmls/messages/errors/sidebar-items.js
+++ openmls/messages/errors/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["ProposalError",""],["ProposalOrRefTypeError",""],["ProposalQueueError",""],["QueuedProposalError",""]]});
+initSidebarItems({"enum":[["ProposalError",""],["ProposalOrRefTypeError",""]]});
