c94d9856c0e02a5342a1a675f4451f0d46bd5542
raphaelrobert <git@raphaelrobert.com>
2022-01-13 14:50:38 +0100

    Introduce thiserror

--- openmls/src/credentials/errors.rs
+++ openmls/src/credentials/errors.rs
@@ -1,17 +1,15 @@
-use crate::config::ConfigError;
 use openmls_traits::types::CryptoError;
+use thiserror::Error;
 use tls_codec::Error as TlsCodecError;
 
-implement_error! {
-    pub enum CredentialError {
-        Simple {
-            UnsupportedCredentialType = "Unsupported credential type.",
-            InvalidSignature = "Invalid signature.",
-        }
-        Complex {
-            ConfigError(ConfigError) = "See `ConfigError` for details.",
-            CryptoError(CryptoError) = "See `CryptoError` for details.",
-            CodecError(TlsCodecError) = "See [`tls_codec::Error`] for details.",
-        }
-    }
+#[derive(Error, Debug, PartialEq, Clone)]
+pub enum CredentialError {
+    #[error("Unsupported credential type.")]
+    UnsupportedCredentialType,
+    #[error("Invalid signature.")]
+    InvalidSignature,
+    #[error(transparent)]
+    CryptoError(#[from] CryptoError),
+    #[error(transparent)]
+    CodecError(#[from] TlsCodecError),
 }
