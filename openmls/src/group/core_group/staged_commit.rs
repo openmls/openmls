35f74ac740ba53aa444e4982c33e7475ecab27c5
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2022-01-14 14:45:59 +0100

    Allow self-removal during external commit ("resync") (#697)
    This commit changes the external commit logic such that if a client does an external commit for a group and a client is present with the same `identity`, the client will automatically commit an inline remove proposal targeting its former self in the group.
    
    Fixes #682.

--- openmls/src/group/core_group/staged_commit.rs
+++ openmls/src/group/core_group/staged_commit.rs
@@ -150,7 +150,11 @@ impl CoreGroup {
             // there are no blanks and the new member extended the tree to
             // fit in.
             if apply_proposals_values.external_init_secret_option.is_some() {
-                diff.add_leaf(key_package.clone())?;
+                let sender_leaf_index = diff.add_leaf(key_package.clone())?;
+                // The new member should have the same index as the claimed sender index.
+                if sender_leaf_index != mls_plaintext.sender_index() {
+                    return Err(StageCommitError::InconsistentSenderIndex.into());
+                }
             }
 
             // Decrypt the UpdatePath
