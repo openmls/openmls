7c5478dae4fc2e62182f5649fd2af8720c25b465
raphaelrobert <git@raphaelrobert.com>
2022-02-02 14:37:23 +0100

    Clean up some more clippy warnings

--- openmls/src/credentials/errors.rs
+++ openmls/src/credentials/errors.rs
@@ -2,12 +2,16 @@ use thiserror::Error;
 
 use crate::error::LibraryError;
 
+/// An error that occurs in methods of a [`Credential`].
 #[derive(Error, Debug, PartialEq, Clone)]
 pub enum CredentialError {
+    /// The type of credential is not supported.
     #[error("Unsupported credential type.")]
     UnsupportedCredentialType,
+    /// The signature that was verified with the credential was invalid.
     #[error("Invalid signature.")]
     InvalidSignature,
+    /// A library error occured.
     #[error(transparent)]
     LibraryError(#[from] LibraryError),
 }
