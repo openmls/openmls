0bd58f9bce0be26541daa472791439e8e2058562
Damian Poddebniak <poddebniak@mailbox.org>
2022-10-24 12:34:01 +0200

    docs: Add presentation language to `KDFLabel`.

--- openmls/src/ciphersuite/kdf_label.rs
+++ openmls/src/ciphersuite/kdf_label.rs
@@ -2,6 +2,15 @@ use super::*;
 
 /// `KdfLabel` is later serialized and used in the `label` field of
 /// `kdf_expand_label`.
+///
+/// ```c
+/// // draft-ietf-mls-protocol-16
+/// struct {
+///     uint16 length = Length;
+///     opaque label<V> = "MLS 1.0 " + Label;
+///     opaque context<V> = Context;
+/// } KDFLabel;
+/// ```
 #[derive(TlsSerialize, TlsSize)]
 pub(in crate::ciphersuite) struct KdfLabel {
     length: u16,
