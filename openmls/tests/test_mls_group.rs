7a26aa4752b0928b0debc8d7277e71809481aff0
Damian Poddebniak <poddebniak@mailbox.org>
2022-10-25 18:42:54 +0200

    refactor: Rename `authentication_code` to `epoch_authenticator`.

--- openmls/tests/test_mls_group.rs
+++ openmls/tests/test_mls_group.rs
@@ -181,10 +181,10 @@ fn mls_group_operations(ciphersuite: Ciphersuite, backend: &impl OpenMlsCryptoPr
         // Make sure that both groups have the same members
         assert_eq!(alice_group.members(), bob_group.members());
 
-        // Make sure that both groups have the same authentication code
+        // Make sure that both groups have the same epoch authenticator
         assert_eq!(
-            alice_group.authentication_code().as_slice(),
-            bob_group.authentication_code().as_slice()
+            alice_group.epoch_authenticator().as_slice(),
+            bob_group.epoch_authenticator().as_slice()
         );
 
         // === Alice sends a message to Bob ===
