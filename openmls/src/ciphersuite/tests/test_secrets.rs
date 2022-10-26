1bc1b6e34091c7a02e7467f5070ce1057b03a9b9
Damian Poddebniak <poddebniak@mailbox.org>
2022-10-26 11:23:55 +0200

    sync: Remove `ProtocolVersion::{Reserved,Mls10Draft11}`.

--- openmls/src/ciphersuite/tests/test_secrets.rs
+++ openmls/src/ciphersuite/tests/test_secrets.rs
@@ -11,23 +11,10 @@ fn secret_init(ciphersuite: Ciphersuite, backend: &impl OpenMlsCryptoProvider) {
     // These two secrets must be incompatible
     let default_secret =
         Secret::random(ciphersuite, backend, None).expect("Not enough randomness.");
-    let draft_secret = Secret::random(ciphersuite, backend, ProtocolVersion::Mls10Draft11)
+    let draft_secret = Secret::random(ciphersuite, backend, ProtocolVersion::Mls10)
         .expect("Not enough randomness.");
 
     let derived_default_secret = default_secret.derive_secret(backend, "my_test_label");
     let derived_draft_secret = draft_secret.derive_secret(backend, "my_test_label");
     assert_ne!(derived_default_secret, derived_draft_secret);
-}
-
-#[should_panic]
-#[apply(ciphersuites_and_backends)]
-fn secret_incompatible(ciphersuite: Ciphersuite, backend: &impl OpenMlsCryptoProvider) {
-    // These two secrets must be incompatible
-    let default_secret =
-        Secret::random(ciphersuite, backend, None).expect("Not enough randomness.");
-    let draft_secret = Secret::random(ciphersuite, backend, ProtocolVersion::Mls10Draft11)
-        .expect("Not enough randomness.");
-
-    // This must panic because the two secrets have incompatible MLS versions.
-    let _default_extracted = default_secret.hkdf_extract(backend, &draft_secret);
 }
