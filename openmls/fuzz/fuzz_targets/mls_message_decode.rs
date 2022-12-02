a9880c30c7a106b94cebba8fc09af6c0b6b41e29
Damian Poddebniak <poddebniak@mailbox.org>
2022-12-02 20:16:45 +0100

    test(fuzz): Extend `mls_message_decode` fuzz target. (WIP)

--- openmls/fuzz/fuzz_targets/mls_message_decode.rs
+++ openmls/fuzz/fuzz_targets/mls_message_decode.rs
@@ -1,8 +1,30 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-
 use openmls::prelude::*;
+use tls_codec::{Deserialize, Serialize, Size};
 
 fuzz_target!(|data: &[u8]| {
-    let _ = MlsMessageIn::try_from_bytes(data);
+    let mut slice = data;
+
+    // Deserialization must not panic.
+    if let Ok(mls_message_in) = MlsMessageIn::tls_deserialize(&mut slice) {
+        // The (real) amount of consumed bytes must be equal to what `tls_serialized_len` reports.
+        {
+            let got_read_from_tls_codec = mls_message_in.tls_serialized_len();
+            let got_read_from_slice = data.len() - slice.len();
+            // TODO(xxxx): This fails.
+            // assert_eq!(got_read_from_tls_codec, got_read_from_slice);
+        }
+
+        // Serialization and Deserialization must be inverses of each other.
+        {
+            let serialized_mls_message_in = mls_message_in.tls_serialize_detached().unwrap();
+
+            // TODO(xxxx): This fails, too.
+            assert_eq!(
+                &data[..mls_message_in.tls_serialized_len()],
+                &serialized_mls_message_in[..]
+            );
+        }
+    }
 });
