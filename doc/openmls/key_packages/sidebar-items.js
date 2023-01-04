cef5569983227cab72021f68ba24d16742c189d9
duesee <duesee@users.noreply.github.com>
2023-01-04 13:01:08 +0000

    deploy: 6ffe9f5f954d5fbb8a0e019f08d1e9ea874776c4

--- doc/openmls/key_packages/sidebar-items.js
+++ doc/openmls/key_packages/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":[["errors","Key Package errors"]],"struct":[["KeyPackage","The key package struct."]]};
+window.SIDEBAR_ITEMS = {"mod":[["errors","Key Package errors"]],"struct":[["KeyPackage","The key package struct."],["KeyPackageBuilder","Builder that helps creating (and configuring) a [`KeyPackage`]."]]};
