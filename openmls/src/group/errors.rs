730df908c36c96169809a5317b8633d373e60be1
Damian Poddebniak <poddebniak@mailbox.org>
2023-01-26 10:49:13 +0100

    Introduce a set of leaf node validation methods using builder pattern. (#1202)
    * feat: Introduce `Lifetime::has_acceptable_range`.
    
    * validation: Introduce leaf node validation methods using builder pattern.
    
    * chore: Make `Lifetime::is_valid` more robust.
    
    * chore: Annotate incomplete/yet unused leaf node validation methods with `TODO(#1186)`.

--- openmls/src/group/errors.rs
+++ openmls/src/group/errors.rs
@@ -80,6 +80,9 @@ pub enum WelcomeError<KeyStoreError> {
     /// This error indicates the public tree is invalid. See [`PublicTreeError`] for more details.
     #[error(transparent)]
     PublicTreeError(#[from] PublicTreeError),
+    /// This error indicates the leaf node is invalid. See [`LeafNodeValidationError`] for more details.
+    #[error(transparent)]
+    LeafNodeValidation(#[from] LeafNodeValidationError),
 }
 
 /// External Commit error
@@ -371,9 +374,9 @@ pub(crate) enum CreateAddProposalError {
     /// See [`LibraryError`] for more details.
     #[error(transparent)]
     LibraryError(#[from] LibraryError),
-    /// The KeyPackage does not support all required extensions.
-    #[error("The KeyPackage does not support all required extensions.")]
-    UnsupportedExtensions,
+    /// See [`LeafNodeValidationError`] for more details.
+    #[error(transparent)]
+    LeafNodeValidation(#[from] LeafNodeValidationError),
 }
 
 /// Exporter error
@@ -485,6 +488,9 @@ pub(crate) enum CreateGroupContextExtProposalError {
     /// See [`ExtensionError`] for more details.
     #[error(transparent)]
     Extension(#[from] ExtensionError),
+    /// See [`LeafNodeValidationError`] for more details.
+    #[error(transparent)]
+    LeafNodeValidation(#[from] LeafNodeValidationError),
 }
 
 /// Error merging a commit.
