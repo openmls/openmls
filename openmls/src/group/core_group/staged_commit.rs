4f17b0b7f681a0c26374bf428c22a2701f4ff6a6
Damian Poddebniak <poddebniak@mailbox.org>
2023-01-30 17:45:03 +0100

    refactor: Rename `TreeInfoTbs` to `TreeInfo`.
    `Tbs` always signals a signature. `TreeInfoTbs` does not have a signature on its own.

--- openmls/src/group/core_group/staged_commit.rs
+++ openmls/src/group/core_group/staged_commit.rs
@@ -5,7 +5,7 @@ use crate::framing::mls_content::FramedContentBody;
 use crate::treesync::errors::TreeSyncAddLeaf;
 use crate::treesync::node::encryption_keys::EncryptionKeyPair;
 use crate::treesync::node::leaf_node::{
-    LeafNodeTbs, OpenMlsLeafNode, TreeInfoTbs, VerifiableLeafNodeTbs,
+    LeafNodeTbs, OpenMlsLeafNode, TreeInfo, VerifiableLeafNodeTbs,
 };
 use crate::treesync::{diff::StagedTreeSyncDiff, treekem::DecryptPathParams};
 
@@ -210,7 +210,7 @@ impl CoreGroup {
                 //       up.
                 let tbs = LeafNodeTbs::from(
                     leaf_node.clone(),
-                    TreeInfoTbs::commit(self.group_id().clone(), sender_index),
+                    TreeInfo::commit(self.group_id().clone(), sender_index),
                 );
                 let verifiable_leaf_node = VerifiableLeafNodeTbs {
                     tbs: &tbs,
