72006b1782a8ec040d253a65a9c6d9815a1e41ff
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2023-02-21 13:56:54 +0100

    revert re-export from OpenMlsRustCrypto

--- openmls_rust_crypto/src/lib.rs
+++ openmls_rust_crypto/src/lib.rs
@@ -4,10 +4,9 @@
 //! OpenMLS.
 
 pub use openmls_memory_keystore::{MemoryKeyStore, MemoryKeyStoreError};
+use openmls_traits::OpenMlsCryptoProvider;
 
 mod provider;
-// Re-export trait.
-pub use openmls_traits::OpenMlsCryptoProvider;
 pub use provider::*;
 
 #[derive(Default, Debug)]
