55eeb08cf6a3d0b7b3f1da692f10543b1ec64b98
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2023-02-27 08:41:18 +0100

    make public message more inspectable

--- openmls/src/framing/public_message.rs
+++ openmls/src/framing/public_message.rs
@@ -132,12 +132,12 @@ impl PublicMessage {
     }
 
     /// Returns the [`ContentType`] of the message.
-    pub(crate) fn content_type(&self) -> ContentType {
+    pub fn content_type(&self) -> ContentType {
         self.content.body.content_type()
     }
 
     /// Get the sender of this message.
-    pub(crate) fn sender(&self) -> &Sender {
+    pub fn sender(&self) -> &Sender {
         &self.content.sender
     }
 
@@ -201,12 +201,12 @@ impl PublicMessage {
     }
 
     /// Get the group epoch.
-    pub(crate) fn epoch(&self) -> GroupEpoch {
+    pub fn epoch(&self) -> GroupEpoch {
         self.content.epoch
     }
 
     /// Get the [`GroupId`].
-    pub(crate) fn group_id(&self) -> &GroupId {
+    pub fn group_id(&self) -> &GroupId {
         &self.content.group_id
     }
 
