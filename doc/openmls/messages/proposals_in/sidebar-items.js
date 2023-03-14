de6dab9573c28381516841cf733eea4969b657b8
raphaelrobert <raphaelrobert@users.noreply.github.com>
2023-03-14 08:50:49 +0000

    deploy: 700cd8bd4e5bf7f8f6df86f571bd1fa0fd1e29e6

---
+++ doc/openmls/messages/proposals_in/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":[["ProposalIn","Proposal."],["ProposalType","MLS Proposal Types"]],"struct":[["AddProposalIn","Add Proposal."],["UpdateProposalIn","Update Proposal."]]};
