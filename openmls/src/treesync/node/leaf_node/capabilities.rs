05fb99694043849f5b26e3d7ad20ca2997f8044f
Damian Poddebniak <poddebniak@mailbox.org>
2023-04-13 12:47:04 +0200

    tests: Add (de)serialization test for `Capabilities`.

--- openmls/src/treesync/node/leaf_node/capabilities.rs
+++ openmls/src/treesync/node/leaf_node/capabilities.rs
@@ -200,3 +200,63 @@ pub(super) fn default_proposals() -> Vec<ProposalType> {
 pub(super) fn default_credentials() -> Vec<CredentialType> {
     vec![CredentialType::Basic]
 }
+
+#[cfg(test)]
+mod tests {
+    use openmls_traits::types::{Ciphersuite, VerifiableCiphersuite};
+    use tls_codec::{Deserialize, Serialize};
+
+    use super::Capabilities;
+    use crate::{
+        credentials::CredentialType, messages::proposals::ProposalType, prelude::ExtensionType,
+        versions::ProtocolVersion,
+    };
+
+    #[test]
+    fn that_unknown_capabilities_are_de_serialized_correctly() {
+        let versions = vec![ProtocolVersion::Mls10, ProtocolVersion::Mls10Draft11];
+        let ciphersuites = vec![
+            Ciphersuite::MLS_128_DHKEMX25519_AES128GCM_SHA256_Ed25519.into(),
+            Ciphersuite::MLS_128_DHKEMP256_AES128GCM_SHA256_P256.into(),
+            Ciphersuite::MLS_128_DHKEMX25519_CHACHA20POLY1305_SHA256_Ed25519.into(),
+            Ciphersuite::MLS_256_DHKEMX448_AES256GCM_SHA512_Ed448.into(),
+            Ciphersuite::MLS_256_DHKEMP521_AES256GCM_SHA512_P521.into(),
+            Ciphersuite::MLS_256_DHKEMX448_CHACHA20POLY1305_SHA512_Ed448.into(),
+            Ciphersuite::MLS_256_DHKEMP384_AES256GCM_SHA384_P384.into(),
+            VerifiableCiphersuite::new(0x0000),
+            VerifiableCiphersuite::new(0x0A0A),
+            VerifiableCiphersuite::new(0x7A7A),
+            VerifiableCiphersuite::new(0xF000),
+            VerifiableCiphersuite::new(0xFFFF),
+        ];
+
+        let extensions = vec![
+            ExtensionType::Unknown(0x0000),
+            ExtensionType::Unknown(0xFAFA),
+        ];
+
+        let proposals = vec![ProposalType::Unknown(0x7A7A)];
+
+        let credentials = vec![
+            CredentialType::Basic,
+            CredentialType::X509,
+            CredentialType::Unknown(0x0000),
+            CredentialType::Unknown(0x7A7A),
+            CredentialType::Unknown(0xFFFF),
+        ];
+
+        let expected = Capabilities {
+            versions,
+            ciphersuites,
+            extensions,
+            proposals,
+            credentials,
+        };
+
+        let test_serialized = expected.tls_serialize_detached().unwrap();
+
+        let got = Capabilities::tls_deserialize_exact(&test_serialized).unwrap();
+
+        assert_eq!(expected, got);
+    }
+}
