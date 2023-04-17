91b970632bdb5d49b5be553a1e0f5731d4ffe3c1
Brian Meek <brian@hntlabs.com>
2023-04-17 15:43:52 +0900

    Add free method to MlsClient to match proto (#1349)
    * Add free method to MlsClient to match proto
    
    Signed-off-by: Brian Meek <brian@hntlabs.com>
    
    * Update interop_client/src/main.rs
    
    Co-authored-by: Damian Poddebniak <poddebniak@mailbox.org>
    
    ---------
    
    Signed-off-by: Brian Meek <brian@hntlabs.com>
    Co-authored-by: Franziskus Kiefer <franziskuskiefer@gmail.com>
    Co-authored-by: Damian Poddebniak <poddebniak@mailbox.org>

--- interop_client/src/main.rs
+++ interop_client/src/main.rs
@@ -1124,6 +1124,12 @@ impl MlsClient for MlsClientImpl {
     ) -> Result<Response<ProposalResponse>, Status> {
         todo!()
     }
+
+    async fn free(&self, _request: Request<FreeRequest>) -> Result<Response<FreeResponse>, Status> {
+        log::debug!("Got Free request");
+        let response = FreeResponse {};
+        Ok(Response::new(response))
+    }
 }
 
 #[derive(Parser)]
