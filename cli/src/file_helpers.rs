110b03f070be29656e887efb4fe19ca2e74ab884
Firefly35 <loic.touraine@gmail.com>
2023-09-01 14:57:58 +0200

    fix: file_helpers.rs

--- cli/src/file_helpers.rs
+++ cli/src/file_helpers.rs
@@ -1,10 +1,10 @@
 use std::{env, path::PathBuf};
 
 pub fn get_file_path(prefix: &String, user_name: &String, suffix: Option<String>) -> PathBuf {
-    let output_file_name: String;
+    let mut output_file_name: String = prefix.to_owned() + user_name;
     match suffix {
-        Some(s) => output_file_name = prefix.to_owned() + user_name + &s + ".json",
-        None => output_file_name = prefix.to_owned() + user_name + "_ks.json",
+        Some(s) => output_file_name += &(s.to_owned() + ".json"),
+        None => output_file_name += ".json",
     }
     let tmp_folder = env::temp_dir();
     let ks_path = tmp_folder.join(output_file_name);
