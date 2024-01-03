0d53880f5a0823bfd2ad1a607ad5b1bc4ce2fe17
kkohbrok <kkohbrok@users.noreply.github.com>
2024-01-03 16:19:23 +0000

    deploy: e0356c7aa4b1da77b0b9e8b293b04b895b60ea32

--- doc/implementors/core/convert/trait.Into.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"openmls":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.0/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"openmls/group/struct.MlsGroup.html\" title=\"struct openmls::group::MlsGroup\">MlsGroup</a>&gt; for <a class=\"struct\" href=\"openmls/prelude/struct.SerializedMlsGroup.html\" title=\"struct openmls::prelude::SerializedMlsGroup\">SerializedMlsGroup</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
