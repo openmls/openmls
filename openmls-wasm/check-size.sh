e952ec51169138cbc0b53d72e258d79ff908a567
Jan Winkelmann (keks) <jan@cryspen.com>
2024-03-06 09:04:35 +0100

    print sizes in check-size.sh

--- openmls-wasm/check-size.sh
+++ openmls-wasm/check-size.sh
@@ -20,8 +20,12 @@ gzip_thresh=500000
 
 if [ $raw_size -gt $raw_thresh ]; then
 	die "raw size is too large: $raw_size > $raw_thresh"
+else
+	echo "raw size $raw_size is below threshold $raw_thresh"
 fi
 
 if [ $gzip_size -gt $gzip_thresh ]; then
 	die "gzip'd size is too largs: $gzip_size > $gzip_thresh"
+else
+	echo "gzip'd size $gzip_size is below threshold $gzip_thresh"
 fi
