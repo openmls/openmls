ee060fe6a98324fea6a567feb327265d5ebbca6c
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2024-07-17 08:51:27 +0200

    remove `test_` prefix from test modules

--- openmls/src/group/mls_group/tests_and_kats/mod.rs
+++ openmls/src/group/mls_group/tests_and_kats/mod.rs
@@ -1,11 +1,11 @@
 //! Test and Known Answer Test (KAT) modules for the MLS group.
 
+mod core_group;
+mod create_commit_params;
+mod external_init;
 mod kat_passive_client;
 mod kat_welcome;
-mod test_core_group;
-mod test_create_commit_params;
-mod test_external_init;
-mod test_mls_group;
-mod test_past_secrets;
-mod test_proposals;
+mod mls_group;
+mod past_secrets;
+mod proposals;
 pub(crate) mod utils;
