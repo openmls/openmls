c542da651ed3f3a13f2b0fa703b7c5d444a501ef
Dakota Brink <git@kota.is>
2025-01-23 16:48:26 -0500

    expose staged welcome extensions

--- openmls/src/group/mls_group/creation.rs
+++ openmls/src/group/mls_group/creation.rs
@@ -556,6 +556,11 @@ impl StagedWelcome {
 
         Ok(mls_group)
     }
+
+    /// Get a reference to the public group [`PulblicGroup`] of this [`StagedWelcome`].
+    pub fn public_group(&self) -> &PublicGroup {
+        &self.public_group
+    }
 }
 
 fn keys_for_welcome<Provider: OpenMlsProvider>(
