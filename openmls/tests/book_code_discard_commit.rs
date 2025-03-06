3a178a2cee7c1e137f1a0c539a23fed2f25844d2
wysiwys <clara@cryspen.com>
2025-03-06 17:13:21 +0100

    initial draft of more granular group testing functionality

--- openmls/tests/book_code_discard_commit.rs
+++ openmls/tests/book_code_discard_commit.rs
@@ -41,25 +41,228 @@ fn generate_key_package(
         .unwrap()
     // ANCHOR_END: create_key_package
 }
+
+use std::collections::HashMap;
+type Identity = &'static str;
+
+// This is a sketch of additional test suite functionality
+// TODO: move to separate file or combine with existing test framework
+pub(crate) struct TestingGroups<Provider: OpenMlsProvider + Default> {
+    // data for all groups
+    group_id: GroupId,
+    create_config: MlsGroupCreateConfig, // TODO: revisit
+    // map credentials to (StorageProvider, MlsGroup)
+    // store separately for borrowing reasons
+    groups: HashMap<Identity, MlsGroup>,
+    providers: HashMap<Identity, Provider>,
+    signers: HashMap<Identity, SignatureKeyPair>,
+    state: TestingGroupsState,
+}
+
+#[derive(Debug)]
+enum TestingGroupsState {
+    Ready,
+    PendingWelcomeFor {
+        identity: Identity,
+        sender: Identity,
+        welcome: MlsMessageOut,
+        commit: MlsMessageOut,
+    },
+    ReadyToApplyCommit {
+        commit: MlsMessageOut,
+        sender: Identity,
+        welcome_receiver: Option<Identity>,
+    },
+}
+
+impl<Provider: OpenMlsProvider + Default> TestingGroups<Provider> {
+    pub(crate) fn add_member(&mut self, adder: Identity, to_add: Identity) {
+        self.stage_commit_add_member(adder, to_add);
+        self.apply_welcome(None);
+        self.apply_sent_commit();
+    }
+    pub(crate) fn stage_commit_add_member(
+        &mut self,
+        adder: Identity,
+        to_add: Identity,
+        //TODO: more config, e.g. Option<KeyPackage>
+    ) {
+        let ciphersuite = self.create_config.ciphersuite();
+        let to_add_provider = Provider::default();
+
+        // Generate credentials with keys
+        let (credential, to_add_signer) = generate_credential(
+            to_add.into(),
+            ciphersuite.signature_algorithm(),
+            &to_add_provider,
+        );
+
+        // Generate KeyPackages
+        let key_package = generate_key_package(
+            ciphersuite,
+            credential.clone(),
+            Extensions::default(),
+            &to_add_provider,
+            &to_add_signer,
+        );
+
+        let adder_group = self.groups.get_mut(&adder).unwrap();
+        let adder_provider = self.providers.get_mut(&adder).unwrap();
+        let adder_signer = self.signers.get_mut(&adder).unwrap();
+
+        let (commit, welcome, _group_info) = adder_group
+            .add_members(
+                adder_provider,
+                adder_signer,
+                &[key_package.key_package().clone()],
+            )
+            .expect("Could not add members.");
+
+        // keep track of new member info
+        self.providers.insert(to_add, to_add_provider);
+        self.signers.insert(to_add, to_add_signer);
+
+        self.state = TestingGroupsState::PendingWelcomeFor {
+            identity: to_add,
+            sender: adder,
+            welcome,
+            commit: commit.clone(),
+        };
+    }
+    pub(crate) fn apply_welcome(&mut self, join_config: Option<MlsGroupJoinConfig>) {
+        if let TestingGroupsState::PendingWelcomeFor {
+            identity,
+            sender,
+            welcome,
+            commit,
+        } = &self.state
+        {
+            assert!(self.signers.contains_key(identity));
+            assert!(!self.groups.contains_key(identity));
+
+            let provider = self.providers.get(identity).unwrap();
+
+            let welcome: MlsMessageIn = welcome.clone().into();
+            let welcome = welcome
+                .into_welcome()
+                .expect("expected the message to be a welcome message");
+
+            let staged_join = StagedWelcome::new_from_welcome(
+                provider,
+                &join_config.unwrap_or_default(),
+                welcome,
+                None,
+            )
+            .expect("Error constructing staged join");
+            let group = staged_join
+                .into_group(provider)
+                .expect("Error joining group from StagedWelcome");
+
+            self.groups.insert(identity, group);
+
+            // update state
+            self.state = TestingGroupsState::ReadyToApplyCommit {
+                sender,
+                welcome_receiver: Some(identity),
+                commit: commit.clone(),
+            };
+        } else {
+            panic!("Cannot apply Welcome: no Welcome sent");
+        }
+    }
+    pub(crate) fn apply_sent_commit(&mut self) {
+        if let TestingGroupsState::ReadyToApplyCommit {
+            sender,
+            welcome_receiver,
+            commit,
+        } = &self.state
+        {
+            for (member_identity, group) in self.groups.iter_mut() {
+                match (welcome_receiver, member_identity) {
+                    (Some(identity), member_identity) if identity == member_identity => {}
+                    (_, member_identity) if member_identity == sender => {
+                        let provider = self.providers.get(member_identity).unwrap();
+                        group.merge_pending_commit(provider).unwrap();
+                    }
+                    (_, member_identity) => {
+                        //apply the commit
+                        let provider = self.providers.get(member_identity).unwrap();
+
+                        let processed_message = group
+                            .process_message(
+                                provider,
+                                commit.clone().into_protocol_message().unwrap(),
+                            )
+                            .unwrap();
+
+                        if let ProcessedMessageContent::StagedCommitMessage(staged_commit) =
+                            processed_message.into_content()
+                        {
+                            group.merge_staged_commit(provider, *staged_commit).unwrap();
+                        } else {
+                            panic!("Message is incorrect");
+                        }
+                    }
+                }
+            }
+        } else {
+            panic!("No commit sent");
+        }
+
+        self.state = TestingGroupsState::Ready;
+    }
+    pub(crate) fn single_from_identity(
+        group_id: GroupId,
+        identity: Identity,
+        create_config: MlsGroupCreateConfig,
+    ) -> Self {
+        let mut groups = HashMap::new();
+        let mut providers = HashMap::new();
+        let mut signers = HashMap::new();
+
+        let provider = Provider::default();
+
+        let ciphersuite = create_config.ciphersuite();
+
+        // Generate credentials with keys
+        let (credential, signer) = generate_credential(
+            identity.into(),
+            ciphersuite.signature_algorithm(),
+            &provider,
+        );
+
+        let group = MlsGroup::new_with_group_id(
+            &provider,
+            &signer,
+            &create_config,
+            group_id.clone(),
+            credential.clone(),
+        )
+        .expect("An unexpected error occurred.");
+
+        groups.insert(identity, group);
+        providers.insert(identity, provider);
+        signers.insert(identity, signer);
+
+        Self {
+            group_id,
+            groups,
+            providers,
+            signers,
+            create_config,
+            state: TestingGroupsState::Ready,
+        }
+    }
+    pub(crate) fn get_group_storage_state(&self, identity: Identity) -> GroupStorageState {
+        let provider = self.providers.get(&identity).unwrap();
+
+        GroupStorageState::from_storage(provider.storage(), &self.group_id)
+    }
+}
+
 #[openmls_test]
 fn discard_commit_add() {
     let group_id = GroupId::from_slice(b"Test Group");
-
-    let alice_provider = &Provider::default();
-    let bob_provider = &Provider::default();
-
-    // Generate credentials with keys
-    let (alice_credential, alice_signature_keys) = generate_credential(
-        "Alice".into(),
-        ciphersuite.signature_algorithm(),
-        alice_provider,
-    );
-
-    let (bob_credential, bob_signature_keys) = generate_credential(
-        "Bob".into(),
-        ciphersuite.signature_algorithm(),
-        bob_provider,
-    );
 
     // Define the MlsGroup configuration
     let mls_group_create_config = MlsGroupCreateConfig::builder()
@@ -67,15 +270,22 @@ fn discard_commit_add() {
         .use_ratchet_tree_extension(true) // NOTE: important
         .build();
 
-    // === Alice creates a group ===
-    let mut alice_group = MlsGroup::new_with_group_id(
-        alice_provider,
-        &alice_signature_keys,
-        &mls_group_create_config,
-        group_id.clone(),
-        alice_credential,
-    )
-    .expect("An unexpected error occurred.");
+    let mut testing_groups =
+        TestingGroups::single_from_identity(group_id.clone(), "Alice", mls_group_create_config);
+
+    let state_before = testing_groups.get_group_storage_state("Alice");
+
+    let alice_group = testing_groups.groups.get_mut("Alice").unwrap();
+    let alice_provider: &Provider = testing_groups.providers.get("Alice").unwrap();
+    let alice_signature_keys = testing_groups.signers.get("Alice").unwrap();
+
+    let bob_provider = &Provider::default();
+
+    let (bob_credential, bob_signature_keys) = generate_credential(
+        "Bob".into(),
+        ciphersuite.signature_algorithm(),
+        bob_provider,
+    );
 
     // Generate KeyPackages
     let bob_key_package = generate_key_package(
@@ -86,14 +296,12 @@ fn discard_commit_add() {
         &bob_signature_keys,
     );
 
-    let state_before = GroupStorageState::from_storage(alice_provider.storage(), &group_id);
-
     // === Alice adds Bob ===
     //ANCHOR: discard_commit_add_setup
     let (_mls_message_out, _welcome, _group_info) = alice_group
         .add_members(
             alice_provider,
-            &alice_signature_keys,
+            alice_signature_keys,
             &[bob_key_package.key_package().clone()],
         )
         .expect("Could not add members.");
@@ -282,77 +490,24 @@ fn discard_commit_update() {
 
 #[openmls_test]
 fn discard_commit_remove() {
-    let alice_provider = &Provider::default();
-    let bob_provider = &Provider::default();
-
-    let group_id = GroupId::from_slice(b"Test Group");
-    // Generate credentials with keys
-    let (alice_credential, alice_signer) = generate_credential(
-        "Alice".into(),
-        ciphersuite.signature_algorithm(),
-        alice_provider,
-    );
-    let (bob_credential, bob_signer) = generate_credential(
-        "Bob".into(),
-        ciphersuite.signature_algorithm(),
-        bob_provider,
-    );
-    // Create a new KeyPackageBundle for Bob
-    let bob_key_package = generate_key_package(
-        ciphersuite,
-        bob_credential,
-        Extensions::default(),
-        bob_provider,
-        &bob_signer,
-    );
-
     // Define the MlsGroup configuration
     let mls_group_create_config = MlsGroupCreateConfig::builder()
         .ciphersuite(ciphersuite)
         .use_ratchet_tree_extension(true) // NOTE: important
         .build();
+    let group_id = GroupId::from_slice(b"Test Group");
 
-    // === Alice creates a group ===
-    let mut alice_group = MlsGroup::new_with_group_id(
-        alice_provider,
-        &alice_signer,
-        &mls_group_create_config,
-        group_id.clone(),
-        alice_credential.clone(),
-    )
-    .expect("An unexpected error occurred.");
-
-    // === Alice adds Bob ===
-    let (_mls_message_out, welcome, _group_info) = alice_group
-        .add_members(
-            alice_provider,
-            &alice_signer,
-            &[bob_key_package.key_package().clone()],
-        )
-        .expect("Could not add members.");
-
-    alice_group
-        .merge_pending_commit(alice_provider)
-        .expect("error merging pending commit");
-
-    let welcome: MlsMessageIn = welcome.into();
-    let welcome = welcome
-        .into_welcome()
-        .expect("expected the message to be a welcome message");
-
-    let staged_join = StagedWelcome::new_from_welcome(
-        bob_provider,
-        &MlsGroupJoinConfig::default(),
-        welcome,
-        None,
-    )
-    .expect("Error constructing staged join");
-    let mut bob_group = staged_join
-        .into_group(bob_provider)
-        .expect("Error joining group from StagedWelcome");
+    // set up group with two members
+    let mut groups =
+        TestingGroups::single_from_identity(group_id, "Alice", mls_group_create_config);
+    groups.add_member("Alice", "Bob");
 
     // save the storage state
-    let state_before = GroupStorageState::from_storage(bob_provider.storage(), &group_id);
+    let state_before = groups.get_group_storage_state("Bob");
+
+    let bob_group = groups.groups.get_mut("Bob").unwrap();
+    let bob_provider: &Provider = groups.providers.get("Bob").unwrap();
+    let bob_signer: &SignatureKeyPair = groups.signers.get("Bob").unwrap();
 
     // Bob removes Alice
 
@@ -369,7 +524,7 @@ fn discard_commit_remove() {
 
     let alice_leaf_node_index = alice_member.index;
     let (_mls_message_out, _welcome, _group_info) = bob_group
-        .remove_members(bob_provider, &bob_signer, &[alice_leaf_node_index])
+        .remove_members(bob_provider, bob_signer, &[alice_leaf_node_index])
         .expect("Could not remove Alice");
 
     // === Delivery service rejected the commit ===
@@ -381,45 +536,33 @@ fn discard_commit_remove() {
         .expect("Could not clear pending commit");
     //ANCHOR_END: discard_commit_remove
 
-    let state_after = GroupStorageState::from_storage(bob_provider.storage(), &group_id);
+    let state_after = groups.get_group_storage_state("Bob");
     assert!(state_before == state_after);
 }
 
 #[openmls_test]
 fn discard_commit_psk() {
-    let alice_provider = &Provider::default();
-
-    let group_id = GroupId::from_slice(b"Test Group");
-    // Generate credentials with keys
-    let (alice_credential, alice_signer) = generate_credential(
-        "Alice".into(),
-        ciphersuite.signature_algorithm(),
-        alice_provider,
-    );
-
     // Define the MlsGroup configuration
     let mls_group_create_config = MlsGroupCreateConfig::builder()
         .ciphersuite(ciphersuite)
         .use_ratchet_tree_extension(true) // NOTE: important
         .build();
+    let group_id = GroupId::from_slice(b"Test Group");
 
-    // === Alice creates a group ===
-    let mut alice_group = MlsGroup::new_with_group_id(
-        alice_provider,
-        &alice_signer,
-        &mls_group_create_config,
-        group_id.clone(),
-        alice_credential.clone(),
-    )
-    .expect("An unexpected error occurred.");
+    // set up group with two members
+    let mut testing_groups =
+        TestingGroups::single_from_identity(group_id, "Alice", mls_group_create_config);
 
-    // save the storage state
-    let state_before = GroupStorageState::from_storage(alice_provider.storage(), &group_id);
+    let alice_provider: &Provider = testing_groups.providers.get("Alice").unwrap();
+    let alice_signer = testing_groups.signers.get("Alice").unwrap();
+
+    let state_before = testing_groups.get_group_storage_state("Alice");
 
     let psk_bytes = vec![1; 32];
     let psk = Psk::External(ExternalPsk::new(psk_bytes.clone()));
     let psk_id = PreSharedKeyId::new(ciphersuite, alice_provider.rand(), psk.clone()).unwrap();
 
+    let alice_group = testing_groups.groups.get_mut("Alice").unwrap();
     // store
     // TODO: is this correct?
     psk_id
@@ -430,13 +573,13 @@ fn discard_commit_psk() {
 
     // Create commit including propose external psk
     let (_message_out, _proposal_ref) = alice_group
-        .propose_external_psk(alice_provider, &alice_signer, psk_id)
+        .propose_external_psk(alice_provider, alice_signer, psk_id)
         .expect("Could not propose adding an external psk");
 
     assert_eq!(alice_group.pending_proposals().count(), 1);
 
     let (_commit, _welcome, _group_info) = alice_group
-        .commit_to_pending_proposals(alice_provider, &alice_signer)
+        .commit_to_pending_proposals(alice_provider, alice_signer)
         .unwrap();
 
     // === Delivery service rejected the commit ===
@@ -454,7 +597,7 @@ fn discard_commit_psk() {
         .expect("Could not clear pending commit");
     //ANCHOR_END: discard_commit_psk
 
-    let state_after = GroupStorageState::from_storage(alice_provider.storage(), &group_id);
+    let state_after = testing_groups.get_group_storage_state("Alice");
     assert!(state_before.non_proposal_state == state_after.non_proposal_state);
 }
 
