032edeaf8bb37d80154026d3cf1994d4d4ff1da5
wysiwys <clara@cryspen.com>
2025-03-12 15:57:39 +0100

    use `thiserror` to avoid boilerplate

--- openmls/src/test_utils/single_group_test_framework/errors.rs
+++ openmls/src/test_utils/single_group_test_framework/errors.rs
@@ -1,3 +1,5 @@
+use thiserror::Error;
+
 use crate::prelude::{commit_builder::*, *};
 pub use crate::utils::*;
 
@@ -10,61 +12,15 @@ pub use openmls_traits::{
 // type alias for `TestError`
 pub type GroupError<Provider> =
     TestError<<<Provider as OpenMlsProvider>::StorageProvider as StorageProviderTrait<CURRENT_VERSION>>::Error>;
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum TestError<StorageError> {
-    CreateCommit(CreateCommitError),
-    CommitBuilderStage(CommitBuilderStageError<StorageError>),
-    NewGroup(NewGroupError<StorageError>),
-    ProcessMessage(ProcessMessageError),
-    Welcome(WelcomeError<StorageError>),
-    ProtocolMessage(ProtocolMessageError),
-    MergeCommit(MergeCommitError<StorageError>),
-    CommitToPendingProposals(CommitToPendingProposalsError<StorageError>),
+    CreateCommit(#[from] CreateCommitError),
+    CommitBuilderStage(#[from] CommitBuilderStageError<StorageError>),
+    NewGroup(#[from] NewGroupError<StorageError>),
+    ProcessMessage(#[from] ProcessMessageError),
+    Welcome(#[from] WelcomeError<StorageError>),
+    ProtocolMessage(#[from] ProtocolMessageError),
+    MergeCommit(#[from] MergeCommitError<StorageError>),
+    CommitToPendingProposals(#[from] CommitToPendingProposalsError<StorageError>),
     NoSuchMember,
-}
-
-impl<StorageError> From<CreateCommitError> for TestError<StorageError> {
-    fn from(e: CreateCommitError) -> Self {
-        TestError::CreateCommit(e)
-    }
-}
-impl<StorageError> From<CommitBuilderStageError<StorageError>> for TestError<StorageError> {
-    fn from(e: CommitBuilderStageError<StorageError>) -> Self {
-        TestError::CommitBuilderStage(e)
-    }
-}
-
-impl<StorageError> From<CommitToPendingProposalsError<StorageError>> for TestError<StorageError> {
-    fn from(e: CommitToPendingProposalsError<StorageError>) -> Self {
-        TestError::CommitToPendingProposals(e)
-    }
-}
-
-impl<StorageError> From<NewGroupError<StorageError>> for TestError<StorageError> {
-    fn from(e: NewGroupError<StorageError>) -> Self {
-        TestError::NewGroup(e)
-    }
-}
-impl<StorageError> From<WelcomeError<StorageError>> for TestError<StorageError> {
-    fn from(e: WelcomeError<StorageError>) -> Self {
-        TestError::Welcome(e)
-    }
-}
-
-impl<StorageError> From<ProcessMessageError> for TestError<StorageError> {
-    fn from(e: ProcessMessageError) -> Self {
-        TestError::ProcessMessage(e)
-    }
-}
-
-impl<StorageError> From<ProtocolMessageError> for TestError<StorageError> {
-    fn from(e: ProtocolMessageError) -> Self {
-        TestError::ProtocolMessage(e)
-    }
-}
-
-impl<StorageError> From<MergeCommitError<StorageError>> for TestError<StorageError> {
-    fn from(e: MergeCommitError<StorageError>) -> Self {
-        TestError::MergeCommit(e)
-    }
 }
