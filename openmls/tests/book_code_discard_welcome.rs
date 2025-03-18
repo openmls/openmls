79f01e63a650908a17186e366d161b8078059377
wysiwys <clara@cryspen.com>
2025-03-18 09:59:39 +0100

    update examples to use test suite

--- openmls/tests/book_code_discard_welcome.rs
+++ openmls/tests/book_code_discard_welcome.rs
@@ -1,8 +1,12 @@
-use openmls::{prelude::*, test_utils::storage_state::GroupStorageState, *};
+use openmls::{
+    prelude::*, test_utils::single_group_test_framework::*,
+    test_utils::storage_state::GroupStorageState, *,
+};
 use openmls_basic_credential::SignatureKeyPair;
 use openmls_test::openmls_test;
 use openmls_traits::{signatures::Signer, types::SignatureScheme};
 
+#[allow(dead_code)]
 fn generate_credential(
     identity: Vec<u8>,
     signature_algorithm: SignatureScheme,
@@ -25,6 +29,7 @@ fn generate_credential(
     )
 }
 
+#[allow(dead_code)]
 fn generate_key_package(
     ciphersuite: Ciphersuite,
     credential_with_key: CredentialWithKey,
@@ -43,23 +48,14 @@ fn generate_key_package(
 
 #[openmls_test]
 fn not_join_group() {
+    // Set up Alice group
+    let alice_party = CorePartyState::<Provider>::new("alice");
+    let bob_party = CorePartyState::<Provider>::new("bob");
+
+    let alice_pre_group = alice_party.generate_pre_group(ciphersuite);
+    let bob_pre_group = bob_party.generate_pre_group(ciphersuite);
+
     let group_id = GroupId::from_slice(b"Test Group");
-
-    let alice_provider = &Provider::default();
-    let bob_provider = &Provider::default();
-
-    // Generate credentials with keys
-    let (alice_credential, alice_signature_keys) = generate_credential(
-        "Alice".into(),
-        ciphersuite.signature_algorithm(),
-        alice_provider,
-    );
-
-    let (bob_credential, bob_signature_keys) = generate_credential(
-        "Bob".into(),
-        ciphersuite.signature_algorithm(),
-        bob_provider,
-    );
 
     // Define the MlsGroup configuration
     let mls_group_create_config = MlsGroupCreateConfig::builder()
@@ -67,35 +63,27 @@ fn not_join_group() {
         .use_ratchet_tree_extension(true) // NOTE: important
         .build();
 
-    // === Alice creates a group ===
-    let mut alice_group = MlsGroup::new_with_group_id(
-        alice_provider,
-        &alice_signature_keys,
-        &mls_group_create_config,
-        group_id.clone(),
-        alice_credential,
-    )
-    .expect("An unexpected error occurred.");
-
+    let mut group_state =
+        GroupState::new_from_party(group_id.clone(), alice_pre_group, mls_group_create_config)
+            .unwrap();
     // Generate KeyPackages
-    let bob_key_package = generate_key_package(
-        ciphersuite,
-        bob_credential.clone(),
-        Extensions::default(),
-        bob_provider,
-        &bob_signature_keys,
-    );
+    let bob_key_package = bob_pre_group.key_package_bundle.key_package().clone();
+
+    let [alice] = group_state.members_mut(&["alice"]);
 
     // === Alice adds Bob ===
-    let (_mls_message_out, welcome, _group_info) = alice_group
+    let (_commit, welcome, _group_info) = alice
+        .group
         .add_members(
-            alice_provider,
-            &alice_signature_keys,
-            &[bob_key_package.key_package().clone()],
+            &alice_party.provider,
+            &alice.party.signer,
+            &[bob_key_package],
         )
-        .expect("Could not add members.");
+        .expect("Could not add Bob");
 
     let welcome: MlsMessageIn = welcome.into();
+
+    let bob_provider = &bob_party.provider;
 
     let group_context: Option<GroupContext> =
         bob_provider.storage().group_context(&group_id).unwrap();
