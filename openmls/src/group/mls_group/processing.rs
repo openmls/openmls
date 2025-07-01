6ff95ac5f888a0a28bdb26f13813735a07b1e945
clara <144733119+wysiwys@users.noreply.github.com>
2025-07-01 08:21:44 -0400

    Support GroupContextExtensions proposals from external senders (#1784)
    * implement `ExternalProposal::new_group_context_extensions()`
    
    * handle `GroupContextExtensions` proposals from external senders
    
    * add tests for unknown extension type
    
    * test for a default extension type
    
    * clean up tests
    
    * always use `QueuedProposal::from_authenticated_content_by_ref()` for external proposals
    
    * move capabilities assertion to setup function
    
    * documentation

--- openmls/src/group/mls_group/processing.rs
+++ openmls/src/group/mls_group/processing.rs
@@ -319,6 +319,24 @@ impl MlsGroup {
                         Err(ProcessMessageError::UnauthorizedExternalApplicationMessage)
                     }
                     // TODO: https://validation.openmls.tech/#valn1502
+                    FramedContentBody::Proposal(Proposal::GroupContextExtensions(_)) => {
+                        let content = ProcessedMessageContent::ProposalMessage(Box::new(
+                            QueuedProposal::from_authenticated_content_by_ref(
+                                self.ciphersuite(),
+                                provider.crypto(),
+                                content,
+                            )?,
+                        ));
+                        Ok(ProcessedMessage::new(
+                            self.group_id().clone(),
+                            self.context().epoch(),
+                            sender,
+                            data,
+                            content,
+                            credential,
+                        ))
+                    }
+
                     FramedContentBody::Proposal(Proposal::Remove(_)) => {
                         let content = ProcessedMessageContent::ProposalMessage(Box::new(
                             QueuedProposal::from_authenticated_content_by_ref(
@@ -338,11 +356,10 @@ impl MlsGroup {
                     }
                     FramedContentBody::Proposal(Proposal::Add(_)) => {
                         let content = ProcessedMessageContent::ProposalMessage(Box::new(
-                            QueuedProposal::from_authenticated_content(
+                            QueuedProposal::from_authenticated_content_by_ref(
                                 self.ciphersuite(),
                                 provider.crypto(),
                                 content,
-                                ProposalOrRefType::Proposal,
                             )?,
                         ));
                         Ok(ProcessedMessage::new(
--- openmls/src/group/public_group/process.rs
+++ openmls/src/group/public_group/process.rs
@@ -16,7 +16,7 @@ use crate::{
         errors::ValidationError, mls_group::errors::ProcessMessageError,
         past_secrets::MessageSecretsStore, proposal_store::QueuedProposal,
     },
-    messages::proposals::{Proposal, ProposalOrRefType},
+    messages::proposals::Proposal,
 };
 
 use super::PublicGroup;
@@ -266,6 +266,24 @@ impl PublicGroup {
                         Err(ProcessMessageError::UnauthorizedExternalApplicationMessage)
                     }
                     // TODO: https://validation.openmls.tech/#valn1502
+                    FramedContentBody::Proposal(Proposal::GroupContextExtensions(_)) => {
+                        let content = ProcessedMessageContent::ProposalMessage(Box::new(
+                            QueuedProposal::from_authenticated_content_by_ref(
+                                self.ciphersuite(),
+                                crypto,
+                                content,
+                            )?,
+                        ));
+                        Ok(ProcessedMessage::new(
+                            self.group_id().clone(),
+                            self.group_context().epoch(),
+                            sender,
+                            data,
+                            content,
+                            credential,
+                        ))
+                    }
+
                     FramedContentBody::Proposal(Proposal::Remove(_)) => {
                         let content = ProcessedMessageContent::ProposalMessage(Box::new(
                             QueuedProposal::from_authenticated_content_by_ref(
@@ -285,11 +303,10 @@ impl PublicGroup {
                     }
                     FramedContentBody::Proposal(Proposal::Add(_)) => {
                         let content = ProcessedMessageContent::ProposalMessage(Box::new(
-                            QueuedProposal::from_authenticated_content(
+                            QueuedProposal::from_authenticated_content_by_ref(
                                 self.ciphersuite(),
                                 crypto,
                                 content,
-                                ProposalOrRefType::Proposal,
                             )?,
                         ));
                         Ok(ProcessedMessage::new(
