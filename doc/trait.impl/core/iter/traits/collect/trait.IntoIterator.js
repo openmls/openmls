3e4ab40d4e1b6703a55284d60e0a92cc398a717e
wysiwys <wysiwys@users.noreply.github.com>
2025-07-03 09:04:54 +0000

    deploy: f67c5862d3980f241587935b2e3a67f61862c6ad

---
+++ doc/trait.impl/core/iter/traits/collect/trait.IntoIterator.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["openmls",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"openmls/group/struct.CommitMessageBundle.html\" title=\"struct openmls::group::CommitMessageBundle\">CommitMessageBundle</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[363]}
