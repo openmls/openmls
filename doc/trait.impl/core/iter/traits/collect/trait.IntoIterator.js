35d97d45330a3fef1a253d89c235ed6707ff11e7
franziskuskiefer <franziskuskiefer@users.noreply.github.com>
2025-08-11 07:50:00 +0000

    deploy: 67eb103386f37a04d31999eb031a8f49d852b870

--- doc/trait.impl/core/iter/traits/collect/trait.IntoIterator.js
+++ doc/trait.impl/core/iter/traits/collect/trait.IntoIterator.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["openmls",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"openmls/group/struct.CommitMessageBundle.html\" title=\"struct openmls::group::CommitMessageBundle\">CommitMessageBundle</a>"]]]]);
+    var implementors = Object.fromEntries([["openmls",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"openmls/group/struct.CommitMessageBundle.html\" title=\"struct openmls::group::CommitMessageBundle\">CommitMessageBundle</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
