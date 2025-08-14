c06d698d5c8da19e653f10d943087a8cbcd7d669
Timm Behner <timm.behner@gmail.com>
2025-08-14 14:46:33 +0200

    adding the hmac function to OpenMlsCrypto
    Since `OpenMlsCrypto` is also used by `Mac` and `Secret` we also need to
    add the respective methods here.
    
    Both the rust_crypto and libcrux crypto providers have been updated to
    implement the new trait. Although `hkdf_expand` could have been used, we
    opted for the explicit `hmac` implementations provided by both crates for
    clarity and consistency.
    
    A separate module was created for the HMAC implementation in rust_crypto
    to resolve import conflicts (multiple applicable methods in the same
    scope).
    
    The test_utils have also been updated to use the new hmac
    function.

--- openmls/src/ciphersuite/secret.rs
+++ openmls/src/ciphersuite/secret.rs
@@ -101,6 +101,25 @@ impl Secret {
         })
     }
 
+    pub(crate) fn hmac<'a>(
+        &self,
+        crypto: &impl OpenMlsCrypto,
+        ciphersuite: Ciphersuite,
+        ikm: impl Into<&'a Secret>,
+    ) -> Result<Self, CryptoError> {
+        log::trace!("HMAC with");
+        log_crypto!(trace, "  salt: {:x?}", self.value);
+        log_crypto!(trace, "  message:  {:x?}", ikm.value);
+
+        Ok(Self {
+            value: crypto.hmac(
+                ciphersuite.hash_algorithm(),
+                self.value.as_slice(),
+                ikm.into().as_slice(),
+            )?,
+        })
+    }
+
     /// HKDF expand where `self` is `prk`.
     pub(crate) fn hkdf_expand(
         &self,
