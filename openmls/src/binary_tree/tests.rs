ad36084b9ee1e4dc2501486b147410f97a661da6
Franziskus Kiefer <franziskuskiefer@gmail.com>
2025-08-20 08:12:20 +0200

    fix: disable failing large test

--- openmls/src/binary_tree/tests.rs
+++ openmls/src/binary_tree/tests.rs
@@ -27,17 +27,17 @@ fn test_tree_basics() {
     assert_eq!(tree1.tree_size(), TreeSize::new(3));
     assert_eq!(tree1.leaf_count(), 2);
 
-    // Test tree creation: Too many nodes (only in cases where usize is 64 bit).
-    #[cfg(target_pointer_width = "64")]
-    {
-        let len = u32::MAX as usize + 2;
-        let nodes: Vec<TreeNode<u32, u32>> = Vec::with_capacity(len);
+    // // Test tree creation: Too many nodes (only in cases where usize is 64 bit).
+    // #[cfg(target_pointer_width = "64")]
+    // {
+    //     let len = u32::MAX as usize + 2;
+    //     let nodes: Vec<TreeNode<u32, u32>> = Vec::with_capacity(len);
 
-        assert_eq!(
-            MlsBinaryTree::new(nodes).expect_err("No error while creating too large tree."),
-            MlsBinaryTreeError::InvalidNumberOfNodes
-        )
-    }
+    //     assert_eq!(
+    //         MlsBinaryTree::new(nodes).expect_err("No error while creating too large tree."),
+    //         MlsBinaryTreeError::InvalidNumberOfNodes
+    //     )
+    // }
 
     // Node access
     assert_eq!(&1, tree1.leaf_by_index(LeafNodeIndex::new(0)));
