a54b67d1a21c9f49901d57a9ab757b2a171fc859
wysiwys <clara@cryspen.com>
2025-08-21 10:03:22 +0200

    update doc comment

--- openmls/src/extensions/application_id_extension.rs
+++ openmls/src/extensions/application_id_extension.rs
@@ -6,7 +6,7 @@ use super::{Deserialize, Serialize};
 ///
 /// Within MLS, a KeyPackage is identified by its hash ([`KeyPackageRef`](`crate::ciphersuite::hash_ref::KeyPackageRef`)).
 /// The application id extension allows applications to add an explicit,
-/// application-defined identifier to a KeyPackage.
+/// application-defined identifier to a [`LeafNode`].
 #[derive(
     PartialEq,
     Eq,
