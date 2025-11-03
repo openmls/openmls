a93d1789b1ca6447195fa2b96ff478b953238af5
wysiwys <clara@cryspen.com>
2025-11-03 18:33:05 +0100

    add API to retrieve queued AppEphemeral proposals from StagedCommit proposal queue

--- openmls/src/group/mls_group/app_ephemeral.rs
+++ openmls/src/group/mls_group/app_ephemeral.rs
@@ -1,6 +1,8 @@
+use super::proposal_store::{ProposalQueue, QueuedAppEphemeralProposal};
 use super::staged_commit::StagedCommitState;
 
-use crate::extensions::AppDataDictionaryExtension;
+use crate::extensions::{AppDataDictionaryExtension, ComponentId};
+use crate::prelude::ProposalType;
 
 impl StagedCommitState {
     /// Return a mutable reference to the [`AppDataDictionaryExtension`], if it exists
@@ -8,5 +10,17 @@ impl StagedCommitState {
         self.group_context_mut()
             .extensions_mut()
             .app_data_dictionary_mut()
+    }
+}
+
+impl ProposalQueue {
+    /// Return an iterator over the [`QueuedAppEphemeralProposal`]s in the proposal queue,
+    /// in the order they appear in the commit.
+    pub fn app_ephemeral_proposals_for_component_id(
+        &self,
+        component_id: ComponentId,
+    ) -> impl Iterator<Item = QueuedAppEphemeralProposal<'_>> {
+        self.app_ephemeral_proposals()
+            .filter(move |p| p.app_ephemeral_proposal().component_id == component_id)
     }
 }
