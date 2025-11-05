f3bf4e7367a3e67c46f49572a4f26b0c83eb78e5
wysiwys <clara@cryspen.com>
2025-11-05 13:38:01 +0100

    return `Vec` from `unique_component_ids_for_app_ephemeral()`

--- openmls/src/group/mls_group/app_ephemeral.rs
+++ openmls/src/group/mls_group/app_ephemeral.rs
@@ -18,12 +18,13 @@ impl ProposalQueue {
 
     /// Return the list of all [`ComponentId`]s available across all
     /// [`QueuedAppEphemeralProposal`]s in the proposal queue.
-    pub fn unique_component_ids_for_app_ephemeral(&self) -> impl Iterator<Item = ComponentId> {
+    pub fn unique_component_ids_for_app_ephemeral(&self) -> Vec<ComponentId> {
+        // sort and deduplicate
         let ids: BTreeSet<_> = self
             .app_ephemeral_proposals()
             .map(|p| p.app_ephemeral_proposal().component_id())
             .collect();
 
-        ids.into_iter()
+        ids.into_iter().collect()
     }
 }
