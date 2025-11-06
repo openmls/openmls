33f3c34d759a5895aceb3f8bb6755637e5800e03
wysiwys <clara@cryspen.com>
2025-11-06 08:39:00 +0100

    rename test

--- openmls/src/messages/proposals.rs
+++ openmls/src/messages/proposals.rs
@@ -574,25 +574,6 @@ pub struct AppEphemeralProposal {
     /// Application data.
     data: VLBytes,
 }
-#[cfg(feature = "extensions-draft-08")]
-impl AppEphemeralProposal {
-    /// Create a new [`AppEphemeralProposal`].
-    pub fn new(component_id: ComponentId, data: Vec<u8>) -> Self {
-        Self {
-            component_id,
-            data: data.into(),
-        }
-    }
-    /// Returns the `component_id` contained in the proposal.
-    pub fn component_id(&self) -> ComponentId {
-        self.component_id
-    }
-
-    /// Returns the `data` contained in the proposal.
-    pub fn data(&self) -> &[u8] {
-        self.data.as_slice()
-    }
-}
 
 #[cfg(feature = "extensions-draft-08")]
 impl AppEphemeralProposal {
