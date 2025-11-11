a855c3046aaa771586143a15a8d3d11e0aeda7c9
wysiwys <clara@cryspen.com>
2025-11-11 18:26:15 +0100

    comment out ciphersuite and add note

--- libcrux_crypto/src/crypto.rs
+++ libcrux_crypto/src/crypto.rs
@@ -57,7 +57,8 @@ impl OpenMlsCrypto for CryptoProvider {
         vec![
             Ciphersuite::MLS_128_DHKEMX25519_AES128GCM_SHA256_Ed25519,
             Ciphersuite::MLS_128_DHKEMX25519_CHACHA20POLY1305_SHA256_Ed25519,
-            Ciphersuite::MLS_128_DHKEMP256_AES128GCM_SHA256_P256,
+            // NOTE: p256 signing is not supported yet
+            // Ciphersuite::MLS_128_DHKEMP256_AES128GCM_SHA256_P256,
             Ciphersuite::MLS_256_XWING_CHACHA20POLY1305_SHA256_Ed25519,
         ]
     }
