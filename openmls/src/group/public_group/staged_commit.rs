8903f82efc88f0ab9e9fa6747f80834094f84836
wysiwys <clara@cryspen.com>
2025-11-12 14:21:42 +0100

    remove unneeded clone

--- openmls/src/group/public_group/staged_commit.rs
+++ openmls/src/group/public_group/staged_commit.rs
@@ -299,7 +299,7 @@ impl PublicGroup {
         };
 
         // Update group context
-        diff.update_group_context(crypto, apply_proposals_values.extensions.clone())?;
+        diff.update_group_context(crypto, apply_proposals_values.extensions)?;
 
         // Update the confirmed transcript hash before we compute the confirmation tag.
         diff.update_confirmed_transcript_hash(crypto, mls_content)?;
