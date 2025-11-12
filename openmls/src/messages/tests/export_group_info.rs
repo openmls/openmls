4481ebcffa88d89a4dcb267dd4a0701acc90044b
wysiwys <clara@cryspen.com>
2025-11-12 14:27:07 +0100

    API for exporting GroupInfo with additional extensions

--- openmls/src/messages/tests/export_group_info.rs
+++ openmls/src/messages/tests/export_group_info.rs
@@ -2,7 +2,8 @@ use tls_codec::{Deserialize, Serialize};
 
 use crate::{
     ciphersuite::signable::Verifiable,
-    group::mls_group::tests_and_kats::utils::setup_alice_group,
+    extensions::{errors::InvalidExtensionError, *},
+    group::{mls_group::tests_and_kats::utils::setup_alice_group, ExportGroupInfoError},
     messages::group_info::{GroupInfo, VerifiableGroupInfo},
     prelude::{ExtensionType, MlsMessageBodyOut},
 };
@@ -51,4 +52,40 @@ fn external_pub_in_group_info() {
     };
 
     assert!(group_info.extensions().contains(ExtensionType::ExternalPub))
+}
+
+/// Tests that extension types are validated correctly when adding to the GroupInfo.
+#[openmls_test::openmls_test]
+fn export_group_info_with_additional_extensions() {
+    // Alice creates a group
+    let (group_alice, _, signer, _pk) = setup_alice_group(ciphersuite, provider);
+
+    // The GroupInfo can't contain these extensions
+    // See https://www.rfc-editor.org/rfc/rfc9420.html#section-17.3-4
+    let application_id_extension = Extension::ApplicationId(ApplicationIdExtension::new(&[]));
+    let required_capabilities_extension =
+        Extension::RequiredCapabilities(RequiredCapabilitiesExtension::new(&[], &[], &[]));
+    let external_senders_extension = Extension::ExternalSenders(vec![]);
+
+    let invalid_extensions = [
+        application_id_extension,
+        required_capabilities_extension,
+        external_senders_extension,
+    ];
+
+    for extension in invalid_extensions {
+        let err = group_alice
+            .export_group_info_with_additional_extensions(
+                provider.crypto(),
+                &signer,
+                true,
+                Some(extension),
+            )
+            .unwrap_err();
+
+        assert!(matches!(
+            err,
+            ExportGroupInfoError::InvalidExtensionError(InvalidExtensionError::IllegalInGroupInfo)
+        ));
+    }
 }
