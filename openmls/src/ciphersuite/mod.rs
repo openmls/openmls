33e1c5c82604216d00e6cfcb78e686247ea6d859
Franziskus Kiefer <franziskuskiefer@gmail.com>
2025-11-25 15:24:40 +0100

    fixup: feature guard

--- openmls/src/ciphersuite/mod.rs
+++ openmls/src/ciphersuite/mod.rs
@@ -25,6 +25,7 @@ mod secret;
 pub mod hash_ref;
 pub mod signable;
 pub mod signature;
+#[cfg(feature = "extensions-draft-08")]
 pub use hpke::{
     safe_decrypt_with_label, safe_encrypt_with_label, Error as HpkeError, SafeEncryptionContext,
 };
