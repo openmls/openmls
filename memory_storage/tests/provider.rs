6983189dce19504c178aea3e66aec31aa3b95e67
wysiwys <clara@cryspen.com>
2026-01-08 17:50:08 +0100

    update example

---
+++ memory_storage/tests/provider.rs
@@ -0,0 +1,43 @@
+use openmls_libcrux_crypto::Provider;
+use openmls_memory_storage::MemoryStorageManager;
+use openmls_traits::{storage::*, OpenMlsProvider};
+
+// Test type
+#[derive(serde::Serialize, serde::Deserialize, PartialEq, Eq, Debug, Clone)]
+struct TestGroupId(Vec<u8>);
+impl traits::GroupId<CURRENT_VERSION> for TestGroupId {}
+impl Key<CURRENT_VERSION> for TestGroupId {}
+
+// Test type
+#[derive(serde::Serialize, serde::Deserialize, PartialEq, Eq, Debug, Clone)]
+enum TestGroupState {
+    Missing,
+    Operational,
+}
+impl traits::GroupState<CURRENT_VERSION> for TestGroupState {}
+impl Entity<CURRENT_VERSION> for TestGroupState {}
+
+#[tokio::test]
+async fn basic_example() {
+    // initialize a new provider
+    let provider = Provider::default();
+    let my_id = TestGroupId(b"my_id".to_vec());
+
+    // get a lock handle for the new id
+    let handle = provider.storage_manager().get_handle(&my_id).unwrap();
+    {
+        // acquire a lock
+        let storage_provider = handle.lock().await;
+        storage_provider
+            .write_group_state(&TestGroupState::Operational)
+            .unwrap();
+
+        // check that the data was updated correctly
+        assert!(matches!(
+            storage_provider.group_state().unwrap(),
+            Some(TestGroupState::Operational)
+        ));
+
+        // the lock is dropped
+    }
+}
