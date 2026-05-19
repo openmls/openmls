38211631957261c5292addb47968568a123688ce
franziskuskiefer <franziskuskiefer@users.noreply.github.com>
2026-05-19 10:50:22 +0000

    deploy: 08339141a044c7657c8c9c61092a660cf906ed1a

--- book/searcher-c2a407aa.js
+++ book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-dab74b33.js',
+                path_to_root + 'searchindex-a037a4a3.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
