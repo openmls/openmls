5dcd8cb386fae172eb06c7194f5acf3232b95c38
clara <144733119+wysiwys@users.noreply.github.com>
2026-05-21 14:43:45 +0200

    allow creating an `AddProposal` from a `KeyPackage` (#2035)

--- openmls/src/messages/proposals.rs
+++ openmls/src/messages/proposals.rs
@@ -380,6 +380,12 @@ impl AddProposal {
     }
 }
 
+impl From<KeyPackage> for AddProposal {
+    fn from(key_package: KeyPackage) -> AddProposal {
+        AddProposal { key_package }
+    }
+}
+
 /// Update Proposal.
 ///
 /// An Update proposal is a similar mechanism to [`AddProposal`] with the
