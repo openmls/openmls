f4e6a414e75d3978464f0391ceafd81ee6464d2b
wysiwys <clara@cryspen.com>
2026-06-26 10:28:51 +0200

    export migration types and update migration functions

--- openmls/src/storage/migration.rs
+++ openmls/src/storage/migration.rs
@@ -1,21 +1,36 @@
 //! Migration helpers for `openmls`
 
 use openmls_storage_migration::*;
-
-/// Migrate all stored `MessageSecretsStore`s values for each `GroupId` key
-pub fn migrate_message_secrets<S: StorageMigrationHelper>(storage: &S) -> Result<(), S::Error> {
-    S::migrate_message_secrets::<
-        crate::group::past_secrets::MessageSecretsStore,
-        crate::group::past_secrets::MessageSecretsStoreCompat,
-        crate::group::GroupId,
-    >(storage)
-}
+use openmls_traits::storage::{traits, Entity, StorageProvider, CURRENT_VERSION};
 
 /// Migrate all stored `MlsGroupState`s  for each `GroupId` key
-pub fn migrate_group_state<S: StorageMigrationHelper>(storage: &S) -> Result<(), S::Error> {
+pub fn migrate_group_state<S: StorageMigrationHelper<CURRENT_VERSION>>(
+    storage: &S,
+) -> Result<
+    (),
+    StorageMigrationError<
+        <S as StorageProvider<CURRENT_VERSION>>::Error,
+        <S as StorageProvider<CURRENT_VERSION>>::Error,
+    >,
+> {
     S::migrate_group_state::<
         crate::group::MlsGroupState,
         crate::group::MlsGroupStateCompat,
         crate::group::GroupId,
     >(storage)
 }
+
+/// Public re-export of MessageSecretsStore
+#[derive(serde::Serialize, serde::Deserialize)]
+#[serde(transparent)]
+pub struct MessageSecretsStore(crate::group::past_secrets::MessageSecretsStore);
+
+/// Public re-export of MessageSecretsStoreCompat
+#[derive(serde::Serialize, serde::Deserialize)]
+#[serde(transparent)]
+pub struct MessageSecretsStoreCompat(crate::group::past_secrets::MessageSecretsStoreCompat);
+
+impl Entity<CURRENT_VERSION> for MessageSecretsStore {}
+impl traits::MessageSecrets<CURRENT_VERSION> for MessageSecretsStore {}
+impl Entity<CURRENT_VERSION> for MessageSecretsStoreCompat {}
+impl traits::MessageSecrets<CURRENT_VERSION> for MessageSecretsStoreCompat {}
