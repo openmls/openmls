0371bb8f714b1ca4404cb89a8db6aee9bf18e9ca
wysiwys <clara@cryspen.com>
2026-07-13 10:32:29 +0200

    add API to `ProcessedMessage` to retrieve the own key package

--- openmls/src/group/mls_group/creation.rs
+++ openmls/src/group/mls_group/creation.rs
@@ -25,7 +25,6 @@ use crate::{
     },
 };
 
-#[cfg(doc)]
 use crate::key_packages::KeyPackage;
 
 impl MlsGroup {
@@ -509,6 +508,14 @@ impl ProcessedWelcome {
             .exporter_secret()
             .derive_exported_secret(self.ciphersuite, crypto, label, context, key_length)
             .map_err(LibraryError::unexpected_crypto_error)?)
+    }
+
+    /// Retrieve a reference to the own [`KeyPackage`] used
+    /// to build the group
+    pub fn own_key_package(&self) -> Option<&KeyPackage> {
+        self.key_material
+            .key_package_bundle()
+            .map(|bundle| bundle.key_package())
     }
 }
 
