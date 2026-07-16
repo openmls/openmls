debce69e0bc752e4ad097b723e469e6c3364aa1e
boxdot <dimitri@phnx.im>
2026-07-16 15:17:56 +0200

    also increase the gzip limit

--- openmls-wasm/check-size.sh
+++ openmls-wasm/check-size.sh
@@ -16,7 +16,7 @@ raw_size=$(tar c pkg | wc -c)
 gzip_size=$(tar cj pkg | wc -c)
 
 raw_thresh=2000000
-gzip_thresh=610000
+gzip_thresh=650000
 
 if [ $raw_size -gt $raw_thresh ]; then
 	die "raw size is too large: $raw_size > $raw_thresh"
